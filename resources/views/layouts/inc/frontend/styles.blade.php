9a7f3608814f0a48105d2fc8719da494a6197cd3
Arso991 <arsogn991@gmail.com>
2025-07-30 20:08:21 +0100

    secure asset

--- resources/views/layouts/inc/frontend/styles.blade.php
+++ resources/views/layouts/inc/frontend/styles.blade.php
@@ -8,19 +8,19 @@
 {{-- <link id="style" href="{{ asset('assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" /> --}}
 
 <!-- STYLE CSS -->
-<link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
-<link href="{{ asset('assets/css/dark-style.css') }}" rel="stylesheet" />
-<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+<link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet" />
+<link href="{{ secure_asset('assets/css/dark-style.css') }}" rel="stylesheet" />
+<link rel="stylesheet" href="{{ secure_asset('assets/css/custom.css') }}">
 
 <!--- FONT-ICONS CSS -->
-<link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" />
+<link href="{{ secure_asset('assets/css/icons.css') }}" rel="stylesheet" />
 
 <!-- COLOR SKIN CSS -->
 <link id="theme" rel="stylesheet" type="text/css" media="all" href="../assets/colors/color1.css" />
 
 <!-- INTERNAL Switcher css -->
-<link href="{{ asset('assets/switcher/css/switcher.css') }}" rel="stylesheet" />
-<link href="{{ asset('assets/switcher/demo.css') }}" rel="stylesheet" />
+<link href="{{ secure_asset('assets/switcher/css/switcher.css') }}" rel="stylesheet" />
+<link href="{{ secure_asset('assets/switcher/demo.css') }}" rel="stylesheet" />
 
 {{-- SWEET ALERTE --}}
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
